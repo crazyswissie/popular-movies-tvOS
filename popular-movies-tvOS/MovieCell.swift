61e7037e9a92c95d2214635d64f77cec6d504f7a
Nico Pauli <nico.pauli@gmail.com>
2016-04-08 10:19:38 -0700

    repo creation

---
+++ popular-movies-tvOS/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  popular-movies-tvOS
+//
+//  Created by Pauli, Nico on 4/8/16.
+//  Copyright © 2016 seventhree. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var movieLbl: UILabel!
+    
+    func configureCell() {
+        
+    }
+    
+    
+}
