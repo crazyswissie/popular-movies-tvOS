3c5c71a8453cd8673b5b1938649714e16d51bd7c
Nico Pauli <nico.pauli@gmail.com>
2016-04-11 13:47:31 -0700

    some progress

--- popular-movies-tvOS/ViewController.swift
+++ popular-movies-tvOS/ViewController.swift
@@ -8,18 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    let URL_BASE = "https://api.themoviedb.org/3/movie/popular?api_key=edb6baa1338cedef62dc5593d29538ff"
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies = [Movie]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        downloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func downloadData() {
+        let url = NSURL(string: URL_BASE)!
+        let request = NSURLRequest(URL: url)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) ->
+            Void in
+            
+            if error != nil {
+                print(error.debugDescription)
+            } else {
+                do {
+                    let dict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? Dictionary<String, AnyObject>
+                    
+                    if let results = dict!["results"] as? [Dictionary<String, AnyObject>] {
+                        
+                        for obj in results {
+                            let movie = Movie(movieDict: obj)
+                            self.movies.append(movie)
+                        }
+                        
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.collectionView.reloadData()
+                        }
+                    }
+                    
+                } catch {
+                    
+                }
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
+
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView  .dequeueReusableCellWithReuseIdentifier("MovieCell", forIndexPath: indexPath) as? MovieCell {
+            
+            let movie = movies[indexPath.row]
+            cell.configureCell(movie)
+            
+            return cell
+        } else {
+            return MovieCell()
+        }
+
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        return movies.count
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(251, 343)
     }
 
+    
 
 }
 
